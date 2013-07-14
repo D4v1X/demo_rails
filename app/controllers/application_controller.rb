b4bc8357379dff1ab787444898d0fa704a49ec6b
David Santiago Barrera <davix.sb@gamil.com>
2013-07-14 20:25:23 +0100

    Shopping cart created.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
     return Time.now
   end
 
+  private
+
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
 end
